2b872dd0253b855585b78c61f10a91089e94b172
Bill Kable <bkable@gmail.com>
2020-04-25 02:03:53 -0500

    start point for distributed-app lab

---
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker.restsupport;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public RestOperations restOperations() {
+        return new RestTemplate();
+    }
+
+}
