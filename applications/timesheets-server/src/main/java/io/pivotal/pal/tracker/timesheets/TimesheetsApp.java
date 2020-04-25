52f95e73189373d86ee13a31bf9eaaa66e78480e
Bill Kable <bkable@gmail.com>
2020-04-25 02:04:02 -0500

    solution point for exception-hystrix lab

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/TimesheetsApp.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/TimesheetsApp.java
@@ -3,12 +3,14 @@ package io.pivotal.pal.tracker.timesheets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
 
 import java.util.TimeZone;
 
+@EnableCircuitBreaker
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
 public class TimesheetsApp {
@@ -26,9 +28,10 @@ public class TimesheetsApp {
     @Bean
     ProjectClient projectClient(
             RestOperations restOperations,
-            @Value("${registration.server.endpoint}") String registrationEndpoint
-    ) {
+            @Value("${registration.server.endpoint}") String registrationEndpoint,
+            ProjectClientCache projectClientCache) {
         return new ProjectClient(restOperations,
-                                registrationEndpoint);
+                                registrationEndpoint,
+                                projectClientCache);
     }
 }
--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,19 +1,32 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
 
 public class ProjectClient {
     private final RestOperations restOperations;
     private final String endpoint;
+    private final ProjectClientCache projectClientCache;
 
     public ProjectClient(RestOperations restOperations,
-                         String registrationServerEndpoint) {
+                         String registrationServerEndpoint,
+                         ProjectClientCache projectClientCache) {
         this.restOperations = restOperations;
         this.endpoint = registrationServerEndpoint;
+        this.projectClientCache = projectClientCache;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+
+        projectClientCache.put(projectId,projectInfo);
+
+        return projectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return projectClientCache.get(projectId);
     }
 
 }
